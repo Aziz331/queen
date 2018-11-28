2a41c0bdd748b1dfda92bf4278ca489c37d2fc9a
Aziz331 <44896829+Aziz331@users.noreply.github.com>
2018-11-29 02:01:22 +0700

    Add files via upload

---
+++ bot.js.js
@@ -0,0 +1,17 @@
+﻿
+client.on('message', message =>{
+
+    if(message.content === '*ping'){
+
+let start = Date.now(); message.channel.send('pong').then(message => { 
+
+message.edit(\``js
+Time taken: ${Date.now() - start} ms
+Discord API: ${client.ping.toFixed(0)} ms
+`);
+
+    });
+
+    }
+
+});
